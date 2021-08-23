594a487a24a85aa5dbe2ece90a110fd1f645d112
FortressOFLions <83042903+FortressOFLions@users.noreply.github.com>
2021-08-23 12:29:33 -0400

    Update models.py

--- models.py
+++ models.py
@@ -15,3 +15,10 @@ class LoginCreds(): # Mongodb Connection information
         self.usr = 'username' 
         self.passwd = 'password'
         self.db =  'database_name'
+        self.col = 'collection_name'
+        
+    def connections(self):
+        client = MongoClient(f'mongodb+srv://{self.usr}:{self.passwd}@cluster0.bkmts.mongodb.net/{self.db}?retryWrites=true&w=majority') # REPLACE THIS WITH YOUR OWN CONNECTION STRING, CAN COPY PASTE DIRECTLY FROM MONGODB ATLAS
+        my_db = client[self.db]
+        my_col = my_db[self.col]
+        return my_db, my_col
